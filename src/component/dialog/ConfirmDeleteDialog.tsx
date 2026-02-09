8c0e887ce0b80873f4dd9f90a3b961e91a4ab4ac
warunthorn27 <warunthorn27@gmail.com>
2026-02-09 11:42:52 +0700

    Update confirm delete dialog

--- src/component/dialog/ConfirmDeleteDialog.tsx
+++ src/component/dialog/ConfirmDeleteDialog.tsx
@@ -1,12 +1,12 @@
 import Modal from "@mui/material/Modal";
 import TrashIcon from "../../assets/svg/trash.svg?react";
+import CloseIcon from "../../assets/svg/close.svg?react";
 
 type Props = {
   open: boolean;
   onClose: () => void;
   onConfirm: () => void;
 };
-
 
 export default function ConfirmDeleteDialog({
   open,
@@ -16,31 +16,34 @@ export default function ConfirmDeleteDialog({
   return (
     <Modal open={open} onClose={onClose}>
       <div className="fixed inset-0 flex items-center justify-center bg-black/40">
-        <div className="w-[420px] bg-white rounded-xl px-8 py-8 text-center relative">
+        <div className="w-[500px] bg-white rounded-xl px-8 py-8 text-center relative">
           {/* Close */}
-          <button className="absolute right-4 top-4 text-xl" onClick={onClose}>
-            ✕
+          <button className="absolute right-4 top-3" onClick={onClose}>
+            <CloseIcon className="w-7 h-7" />
           </button>
 
           {/* Icon */}
-          <div className="mx-auto mb-5 w-20 h-20 rounded-full bg-red-100 flex items-center justify-center">
-            <TrashIcon className="w-10 h-10 text-red-600" />
+          <div className="mx-auto mb-4 w-20 h-20 rounded-full bg-[#FFDFDF] flex items-center justify-center">
+            <TrashIcon className="w-[60px] h-[60px] text-[#E71010]" />
           </div>
 
           {/* Text */}
-          <h2 className="text-xl font-medium mb-2">Delete</h2>
-          <p className="text-gray-500 mb-8">
+          <h2 className="text-lg font-normal mb-2">Delete</h2>
+          <p className="text-[#545454] mb-8 font-light">
             Are you sure you want to{" "}
-            <span className="text-red-600">Delete</span> ?
+            <span className="text-[#C80C0C]">Delete</span> ?
           </p>
 
           {/* Actions */}
           <div className="flex justify-center gap-4">
-            <button className="px-6 py-2 border rounded-md" onClick={onClose}>
+            <button
+              className="w-[100px] px-6 py-2 border rounded-md text-black bg-white border-[#CFCFCF] hover:bg-[#F1F1F1]"
+              onClick={onClose}
+            >
               Close
             </button>
             <button
-              className="px-6 py-2 bg-red-600 text-white rounded-md hover:bg-red-700"
+              className="w-[100px] px-6 py-2 bg-[#E71010] text-white rounded-md hover:bg-[#C80C0C]"
               onClick={onConfirm}
             >
               Delete
